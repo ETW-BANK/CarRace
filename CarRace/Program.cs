090ef14403bec5e2386f8e7ded304eefddc3b0fc
dinozoff1981 <tensaeg@yahoo.com>
2023-12-08 22:09:00 +0100

    winner logic added

--- CarRace/Program.cs
+++ CarRace/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
         
-            Car car1=new Car("ferrari",120);
+            Car car1=new Car("Ferrari",120);
             Car car2 = new Car("BMW", 120);
 
            
@@ -19,7 +19,19 @@
                 t.Join();
                 t2.Join();
 
-            Console.WriteLine($"{car1.Name} & {car2.Name} har avslutat Tävlingen!");
+            Console.WriteLine("\n");
+
+            if (t2.ThreadState==0)
+            {
+                Console.WriteLine($"\u001b[36m{car2.Name}\u001b[0m \u001b[34m HAR VUNNIT TÄVLINGEN\u001b[0m \n");
+            }
+            else
+            {
+                Console.WriteLine($"\u001b[36m{car1.Name}\u001b[0m \u001b[34m HAR VUNNIT TÄVLINGEN\u001b[0m \n");
+
+            }
+
+            Console.WriteLine($"\u001b[32m{car1.Name} & {car2.Name} har avslutat Tävlingen!\u001b[0m");
         }
     }
 }
